e610fe1f15e2bfa59309a732e9f9f7d504a89e3c
Amir_h_Ahmadzade <a.h.a9372@gmail.com>
2023-08-23 16:24:42 +0200

    calling functions in order

--- lab-python-function-Amirhossein.py
+++ lab-python-function-Amirhossein.py
@@ -1,8 +1,8 @@
 products = ["t-shirt", "mug", "hat", "book", "keychain"]
 inventory = {}
 customer_orders = set()
-def initialize_inventory(products):
-  for item in products:
+def initialize_inventory(products_list):
+  for item in products_list:
     inventory[item]= int(input(f"How many {item}s are there in the inventory?"))
   return inventory
 
@@ -18,13 +18,12 @@ def get_customer_orders():
 
 def calculate_order_statistics(inventory, customers_orders):
     total_products_ordered = len(customer_orders)
-    total_productas = sum(inventory.values())
-    percentage_of_products_ordered = round(total_products_ordered / total_productas , 2)
-    order_stat = (total_productas, percentage_of_products_ordered)
+    total_products = sum(inventory.values())
+    percentage_of_products_ordered = round(total_products_ordered / total_products , 2)
+    order_stat = (total_products_ordered, percentage_of_products_ordered)
     return order_stat
 
-
-def update_inventory(invetory, customers_orders):
+def update_inventory(inventory, customers_orders):
    for product in customer_orders:
       inventory[product] -= 1
 
@@ -37,5 +36,14 @@ def print_updated_inventory(inventory):
     print("Updated inventory has:")
     for product,quantity in inventory.items():
         print(f"{product}:{quantity}")
+
+
+
+initialize_inventory(products)
+customer_orders = get_customer_orders()
+order_statistic = calculate_order_statistics(inventory, customer_orders)
+print_order_statistics(order_statistic)
+update_inventory(inventory,customer_orders)
+print_updated_inventory(inventory)
 
 
